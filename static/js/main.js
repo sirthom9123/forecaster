b78a27dbdf4d85a3ca9fb3ade52df4bfe6b386e3
Tshiamo Motshabi <qraircorp@gmail.com>
2021-02-24 09:21:10 +0200

    update js files

--- static/js/main.js
+++ static/js/main.js
@@ -5,16 +5,16 @@ const city = document.querySelector('#map')
 const csrf = document.querySelector('csrfmiddlewaretoken')
 const messages = document.querySelector('.message')
 const saveData = document.querySelector('#saveData')	
+const alertBox = document.querySelector('#messages')
 
 
 
 const handleAlerts = (type, text) => {
-	alertBox.innerHTML = `<div class="alert alert-${type}" role="alert">
-							${text}
-							</div>`
+	alertBox.innerHTML = `<div class="alert alert-${type}" role="alert">${text}</div>`
 	
 }
 $(document).on('submit', mapForm, function(e) {
+    // e.preventDefault();
     $.ajax({
         type:'POST',
         url: "{% url 'home' %}",
@@ -23,8 +23,32 @@ $(document).on('submit', mapForm, function(e) {
             csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
         }),
         success:function(response){
-            document.getElementById("getWeather").hide();
-            document.getElementById("weatherData").show();
+            $("getWeather").hide();
+            $("weatherData").show();
+        },
+        error:function(error) {
+            handleAlerts('danger', 'oops... something went wrong!')
+            setTimeout(() => {
+                alertBox.innerHTML = '';
+            }, 5000)
+        }
+    });
+});
+
+$(document).on('click', saveData, function(e) {
+    $.ajax({
+        type:'POST',
+        url: "{% url 'home' %}",
+        data: JSON.stringify({
+            hourly:$('#hourly').val(),
+            icon:$('#img_icon').val(),
+            temperature:$('#temp').val(),
+            wind:$('#wind').val(),
+            description:$('#desc').val(),
+            csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
+        }),
+        success:function(response){
+            $("container").reset();
         },
         error:function(error) {
             handleAlerts('danger', 'oops... something went wrong!')
--- weather/urls.py
+++ weather/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
@@ -6,6 +7,6 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('save_forecast', views.save_forecast, name='save_forecast'),
     path('history_data/', views.history_data, name='history_data'),
-    path('search_data', views.search_data, name='search_data'),
+    path('search_data', csrf_exempt(views.search_data), name='search_data'),
     path('export_csv', views.export_csv, name='export_csv'),
 ]
